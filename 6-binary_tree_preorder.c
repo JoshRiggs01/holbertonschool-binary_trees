794042380ede1eba7af3fd16ad9d9c4ffa22b66f
JoshRiggs01 <5574@holbertonstudents.com>
2022-12-20 14:09:33 -0800

    adding preorder

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - tree to preorder
+ * @tree: the trr to prorder
+ * @func: void funtion in
+ * Return: void
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
