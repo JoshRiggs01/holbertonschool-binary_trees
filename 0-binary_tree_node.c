03999beb3be4bab8bdcf9bc64042a590431d0068
JoshRiggs01 <5574@holbertonstudents.com>
2022-12-20 08:19:51 -0800

    betty style codding

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_t - function that creates binary tree node
+ * binary_tree_node - function that creates binary tree node
  * @parent: parent node
  * @value: variable to iterate
  * Return: new_node
@@ -10,10 +10,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL)
-	return NULL;
-	new_node -> parent = parent;
-	new_node -> n = value;
-	new_node -> left = NULL;
-	new_node -> right = NULL;
-	return new_node;
+		return (NULL);
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	return (new_node);
 }
