de6e28da5d40141bda6d41761f065b8f26375a75
JoshRiggs01 <5574@holbertonstudents.com>
2022-12-20 14:39:34 -0800

    fixed tree heigh

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,7 +9,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
-	if (tree == NULL)
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 	{
 		return (0);
 	}
@@ -18,10 +18,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	right_height = binary_tree_height(tree->right);
 	if (left_height > right_height)
 	{
-		return (left_height);
+		return (left_height + 1);
 	}
 	else
 	{
-		return (right_height);
+		return (right_height + 1);
 	}
 }
