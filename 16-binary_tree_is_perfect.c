5ee8b723d27fbc4dc3cededb36fe3e5319e2d9e0
JoshRiggs01 <5574@holbertonstudents.com>
2022-12-20 17:01:41 -0800

    fized 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -15,8 +15,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	int left_height = binary_tree_height(tree->left);
-	int right_height = binary_tree_height(tree->right);
+	int left_height;
+	left_height = binary_tree_height(tree->left);
+	int right_height;
+	right_height = binary_tree_height(tree->right);
 
 	if (left_height != right_height)
 	{
